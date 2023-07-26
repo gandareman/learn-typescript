5eeba17a8923f92d70866063c62528b3e175a5c1
memo7748 <memo7748@gmail.com>
2023-07-26 12:52:15 +0900

    'modules'

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -13,3 +13,24 @@ let per = new Per()
 
 // developer = per;
 per = developer;
+
+var add = function(n: number) {
+    // 
+}
+
+var sum = function(a: number, b: number) {
+    // 
+}
+
+sum = add;
+// add = sum;
+
+interface Empty<T> {
+    data: T;
+}
+
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+
+empty1 = empty2;
+empty2 = empty1;
---
+++ class-note/ts-modules/app.ts
@@ -0,0 +1,6 @@
+
+import {} from './types'
+var item: Todo = {
+    title: '할일1',
+    checked: false
+}
